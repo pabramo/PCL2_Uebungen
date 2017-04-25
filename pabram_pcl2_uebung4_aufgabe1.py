5ba6c733ef62967f6108e41563c94ce4b32debf8
Petra Abramovic <Petra Abramovic>
2017-04-25 21:48:41 +0200

    tried with testfile, without glob yet

--- pabram_pcl2_uebung4_aufgabe1.py
+++ pabram_pcl2_uebung4_aufgabe1.py
@@ -1,11 +1,44 @@
 import codecs
+import sys
+#from glob import glob
+from lxml import etree
+from xml.etree import ElementTree as et
 
 
+filename = sys.argv[1]
 
-with open('abcd.txt') as src, open('gigaunique.txt', 'w') as trg:
-    sentence_hashes = set()
-    for sentence in src:
-        sentence_hash = hash(sentence)
-        if sentence_hash not in sentence_hashes:
-            sentence_hashes.add(sentence_hash)
-            trg.write(sentence)
+def freqwords(indir, outfile):
+
+    with open(filename, 'rt') as src, open('gigaunique.txt', 'w') as trg:
+        tree = et.parse(filename)
+
+        for node in tree.iter('s'):
+            lemmata = []
+
+            if len(node) >= 6:
+                for token in node:
+                    lemma = token.attrib.get('lemma')
+                    if lemma == None:
+                        pass
+
+                    else:
+                        lemmata.append(lemma)
+
+            dedupl_hashes = set()
+            counter = 0
+
+            for sentence in lemmata:
+                
+                dedupl_hash = hash(sentence)
+
+                if dedupl_hash  not in dedupl_hashes:
+                    dedupl_hashes.add(dedupl_hash)
+
+                else:
+                    counter += 1
+
+                    trg.write(sentence)
+
+freqwords('testfile.xml', 'gigaunique.txt')
+
+
